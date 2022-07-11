66aa554960337a52ae7c4fd1256b01c2bcac24cd
yuuhi <hainm112123@gmail.com>
2022-07-11 10:21:37 +0700

    update login signup feature

--- public/assets/js/sign-in-up.js
+++ public/assets/js/sign-in-up.js
@@ -5,11 +5,14 @@ document.addEventListener('DOMContentLoaded' , function() {
   var clients = [];
   var idCounterRef = db.collection("counter").doc("idCounter");
   var idCounter;
+  var isLoginRef = db.collection("counter").doc("isLogin");
+  var isLogin;
 
   function Signup() {
     var phoneNumberInput = document.querySelector('.modal__form .signup-phone-number-input').value;
     var passwordInput = document.querySelector('.modal__form .signup-password-input').value;
     var retypePasswordInput = document.querySelector('.modal__form .signup-retype-password-input').value;
+    var usernameInput = document.querySelector('.modal__form .signup-username-input').value;
 
     if (phoneNumberInput.length !== 10) {
       alert('invailid phonenumber');
@@ -23,15 +26,20 @@ document.addEventListener('DOMContentLoaded' , function() {
       alert("retyped password don't match!");
       return ;
     }
+    if (usernameInput.length < 4) {
+      alert('username must be at least 4 character');
+      return;
+    }
     if (clients.find(function(client) {
-      return client.username === phoneNumberInput;
+      return client.username === usernameInput;
     })) {
-      alert('this account already exists!');
+      alert('this username already exists!');
       return;
     }
 
     clientsRef.add({
-      username: phoneNumberInput,
+      username: usernameInput,
+      phoneNumbe: phoneNumberInput,
       password: passwordInput,
       id: idCounter + 1,
     }).then(function() {
@@ -41,23 +49,18 @@ document.addEventListener('DOMContentLoaded' , function() {
         value: idCounter,
       }).then(function() {
         console.log('update id-counter complete');
-      })
+        GoLogin(idCounter);
+      });
     }).catch(function(err) {
       alert("Oops, something's wrong >_<! Please try again.");
       console.log(err);
     });
-  
-    toUserHomePage(account);
   }
 
   function Login() {
-    var phoneNumberInput = document.querySelector('.modal__form .login-phone-number-input').value;
+    var phoneNumberInput = document.querySelector('.modal__form .login-phone-number-input').value; //username
     var passwordInput = document.querySelector('.modal__form .login-password-input').value;
 
-    if (phoneNumberInput.length !== 10) {
-      alert('invailid phonenumber');
-      return;
-    }
     if (passwordInput.length < 4) {
       alert('wrong password!');
       return;
@@ -71,27 +74,65 @@ document.addEventListener('DOMContentLoaded' , function() {
       return; 
     }
 
-    toUserHomePage(account);
+    GoLogin(account.id);
   }
 
-  function toUserHomePage(account) {
-    window.location.href += account.id.toString();
+
+  var headerSinupBtn = document.querySelector('.header__navbar .register-btn');
+  var headerLoginBtn = document.querySelector('.header__navbar .login-btn');
+  var headerUserInfor = document.querySelector('.header__navbar .header__user-infor');
+
+  function Logout() {
+    isLoginRef.update({
+      value: false,
+    }).then(function() {
+      document.location.reload();
+    });
+  }
+
+  function GoLogin(id) {
+    isLoginRef.update({
+      value: true,
+      userId: id,
+    }).then(function() {
+      document.location.reload();
+    });
+  }
+
+  function displayUserHomePage(username) {
+    // document.location.reload();
+    headerLoginBtn.style.display = 'none';
+    headerSinupBtn.style.display = 'none';
+    headerUserInfor.style.display = 'inline-flex';
+    var headerUsername = document.querySelector('.navbar-item__username');
+    headerUsername.innerHTML = username;
   }
   
   async function main() {
-    var [clientsSnap, idCounterSnap] = await Promise.all([
+    var [clientsSnap, idCounterSnap , isLoginSnap] = await Promise.all([
       clientsRef.get(),
       idCounterRef.get(),
+      isLoginRef.get(),
     ]);
     clients = clientsSnap.docs.map(function(doc) {
       return doc.data();
     });
     idCounter = idCounterSnap.data().value;
+    isLogin = isLoginSnap.data();
+
+    if (isLogin.value === true) {
+      var user = clients.find(function(client) {
+        return client.id === isLogin.userId;
+      });
+      displayUserHomePage(user.username);
+    }
   
     var loginBtn = document.querySelector('.modal__form .login-btn');
     var signupBtn = document.querySelector('.modal__form .signup-btn');
-    loginBtn.addEventListener('click' , Login);
+    var logoutBtn = document.querySelector('.navbar-item__user-menu-logout');
+    loginBtn.addEventListener('click', Login);
     signupBtn.addEventListener('click', Signup);
+    logoutBtn.addEventListener('click', Logout);
   }
 
   main();
